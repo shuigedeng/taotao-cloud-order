74bca04d065cc56dc25573df52b63de1bb3c1541
shuigedeng <981376577@qq.com>
2026-02-04 21:16:41 +0800

    开启2026.03

--- taotao-cloud-order-interfaces/src/main/java/com/taotao/cloud/order/interfaces/controller/seller/ReceiptSellerController.java
+++ taotao-cloud-order-interfaces/src/main/java/com/taotao/cloud/order/interfaces/controller/seller/ReceiptSellerController.java
@@ -20,7 +20,7 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.taotao.boot.common.model.result.PageResult;
 import com.taotao.boot.common.model.result.Result;
 import com.taotao.boot.data.mybatis.mybatisplus.MpUtils;
-import com.taotao.boot.security.spring.utils.SecurityUtils;
+import com.taotao.boot.security.spring.support.utils.SecurityUtils;
 import com.taotao.boot.web.request.annotation.RequestLogger;
 import com.taotao.boot.webagg.controller.BusinessController;
 import com.taotao.cloud.order.application.dto.own.order.command.OrderReceiptAddCommand;
